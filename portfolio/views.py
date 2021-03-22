cac8fc4ba551cad22e878c7bdbb6c3555f972d1c
Emmanuel <monotone_briggs@hotmail.com>
2021-03-22 21:27:35 +0000

    added csv export function

--- portfolio/views.py
+++ portfolio/views.py
@@ -6,6 +6,9 @@ from django.contrib import messages
 
 # manages the homepage
 def index(request):
+    import csv 
+    import webbrowser
+
     if request.method == 'POST':
         # email variables
         message_name = request.POST['name']
@@ -15,7 +18,21 @@ def index(request):
 
         # send email
         send_mail(str(message_name) + ' - ' + str(message_subject), message_text, message_email, ['emmanueligbinijesu@gmail.com'], fail_silently=True)
+        
+        csvfile = open("contact.csv", "w")
+        csvwriter = csv.writer(csvfile)
 
+        columns = ['name', 'email', 'subject', 'message']
+
+        with open("contact.csv", 'a') as csv:
+            for col in columns:
+                csvwriter.writerow(col)
+            csvwriter.writerow(message_name)
+            csvwriter.writerow(message_email)
+            csvwriter.writerow(message_subject)
+            csvwriter.writerow(message_text)
+
+        webbrowser.open("contact.csv")
         # display message sent message
         messages.success(request, ('Thanks for getting in touch!'))
         return render(request, 'index.html', {'message_name' : message_name, 'message_text' : message_text, 'message_email' : message_email})
