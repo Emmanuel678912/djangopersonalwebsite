e0ab7cddfd9ad364e42ef9619250b3db7dca6eb3
Emmanuel <monotone_briggs@hotmail.com>
2021-03-29 13:12:08 +0000

    updated settings

--- personalweb/settings.py
+++ personalweb/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY_PORTFOLIO']
+SECRET_KEY = os.environ.get('SECRET_KEY_PORTFOLIO')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -115,7 +115,7 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'emmanueligbinijesu@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASSWORD_PORTFOLIO']
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD_PORTFOLIO')
 EMAIL_PORT = 587
 
 # python -m smtpd -n -c DebuggingServer localhost:1025
